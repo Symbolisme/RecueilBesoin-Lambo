71c31e5ad57619917cef1e8f6688cb01cf5e92b5
Jarod <66060639+Symbolisme@users.noreply.github.com>
2022-01-12 16:17:55 +0100

    Delete ent-script.js

--- ent-script.js
+++
@@ -1,7 +0,0 @@
-function redirect() {
-    window.location.href = document.getElementById('fonction').value;
-    return false;
-}
-
-var prenom = localStorage.getItem('prenom');
-var nom = localStorage.getItem('nom'); 
