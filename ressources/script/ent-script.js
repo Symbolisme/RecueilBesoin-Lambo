13bc9bb10df58c8da8ca9e00d1010f8e98dfc0bf
Jarod <66060639+Symbolisme@users.noreply.github.com>
2022-01-13 12:52:48 +0100

    Add files via upload

--- ressources/script/ent-script.js
+++ ressources/script/ent-script.js
@@ -2,6 +2,3 @@ function redirect() {
     window.location.href = document.getElementById('fonction').value;
     return false;
 }
-
-var prenom = localStorage.getItem('prenom');
-var nom = localStorage.getItem('nom'); 
